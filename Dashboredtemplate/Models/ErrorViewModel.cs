044be90e3cc0110e7026e1808e6b0862170c6276
mangesh20202 <70098884+mangesh20202@users.noreply.github.com>
2025-09-15 14:38:03 +0530

    Project files

---
+++ Dashboredtemplate/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Dashboredtemplate.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
